252592f9e9160074e0183e42fc80b3fddb24f769
sahadatpro <sahadatsays@gmail.com>
2019-06-24 01:38:56 +0600

    init project name wonder quotes.

---
+++ wonder-quotes/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h3
+          class="text-center mt-4 bg-light py-3"
+        >Welcome to Wonder Quotes - Demo Application for Learn to VueJS</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card card-body bg-light">
+          <p
+            class="quote"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, totam?</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card card-body bg-light">
+          <p
+            class="quote"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, totam?</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card card-body bg-light">
+          <p
+            class="quote"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, totam?</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+
+<style>
+.quote {
+  font-family: "Arizonia", cursive;
+  font-size: 2em;
+  font-weight: normal;
+}
+</style>
